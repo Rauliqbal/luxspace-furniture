d33acf22f344ce6608295095f0e9daebdbb9ab80
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-03-09 17:41:43 +0700

    [ADD] Navbar responsive and favicon

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 let list = document.querySelector("ul");
 
 function Menu(e) {
-    e.name === "menu" ? ((e.name = "close"), list.classList.remove("left-[50rem]")) : ((e.name = "menu"), list.classList.add("left-[50rem]"));
+    e.name === "menu" ? ((e.name = "close"), list.classList.add("left-[0px]"), list.classList.remove("left-[50rem]")) : ((e.name = "menu"), list.classList.remove("left-[0px]"), list.classList.add("left-[50rem]"));
 }
