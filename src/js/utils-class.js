77c2ef397a824021a9a7731ba465bb984f8ebee3
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-03-11 21:27:32 +0700

    [ADD]Just Arrived section

--- src/js/utils-class.js
+++
@@ -1,7 +0,0 @@
-export function addClass(e, classes) {
-    e.classList && e.classList.add(...classes.split(""));
-}
-
-export function removeClass(e, classes) {
-    e.classList && e.classList.remove(...classes.split(""));
-}
